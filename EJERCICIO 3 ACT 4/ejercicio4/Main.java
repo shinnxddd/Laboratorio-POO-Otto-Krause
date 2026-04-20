c238a7ea6ab0b1ce93d5921198560e247937d8fd
shin:33 <j4zm1n.t0l3d4n0@gmail.com>
2026-04-20 10:32:44 -0300

    Add files via upload

---
+++ EJERCICIO 3 ACT 4/ejercicio4/Main.java
@@ -0,0 +1,29 @@
+package ejercicio4;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    	Libro l1 = new Libro(1, 1234, "Antoine de Saint-Exupéry", "Ficción");
+        Libro l2 = new Libro(2, 4567, "J.K. Rowling", "Fantasía");
+        Libro l3 = new Libro(3, 7890, "George R.R. Martin", "Fantasía");
+        Libro l4 = new Libro(4, 1010, "George R.R. Martin", "Fantasía");
+        Libro l5 = new Libro(5, 8080, "Shotaro Osaki", "Aprendizaje");
+        Libro l6 = new Libro(6, 4444, "Rubio y Chipa", "Amistad");
+        Libro l7 = new Libro(7, 7770, "Osamu Dazai", "Autodestrucción");
+        Libro l8 = new Libro(8, 1551, "Suzanne Collins", "Ciencia ficción");
+        Libro l9 = new Libro(9, 1473, "Dean R. Knootza", "Romance");
+        Libro l10 = new Libro(10, 1977, "Jean Dane", "Suspenso");
+
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l3);
+        System.out.println(l4);
+        System.out.println(l5);
+        System.out.println(l6);
+        System.out.println(l7);
+        System.out.println(l8);
+        System.out.println(l9);
+        System.out.println(l10);
+    }
+}
