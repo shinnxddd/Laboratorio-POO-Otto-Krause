6b0041586e51cd2a32731a39a7fa733924e85d18
shin:33 <j4zm1n.t0l3d4n0@gmail.com>
2026-06-01 09:53:44 -0300

    Add files via upload

---
+++ Matrices/src/Producto.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Producto {
+	private String nombre;
+	private String codigo;
+	private tring precio;
+	
+	public Producto(String nombre, int codigo, double promedio) {      
+	       this.nombre = nombre;
+	       this.codigo = codigo;
+	       this.precio = precio;
+	       }
+	
+	  public String getnombre() {
+		   	return nombre;
+		   }
+	  
+	  public int codigo() {
+		   	return codigo;
+		   }
+	  
+	  
+	  public double promedio() {
+		   	return codigo;
+		   }
+
+}
